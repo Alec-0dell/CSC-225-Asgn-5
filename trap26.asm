8e261e58d8b1b56abebc6e7bd2d499c173cb4e25
A1ec-0dell <aqodell@calpoly.edu>
2024-02-20 23:59:31 -0800

    Completed

--- trap26.asm
+++ trap26.asm
@@ -11,8 +11,39 @@
 ;  3. Enables keyboard interrupts.
 ;  4. Returns to the second program.
 ;  5. Load state of Program 2
-TRAP26                  ; TODO: Implement this service routine.
-
+TRAP26: ; TODO: Implement this service routine.
+    ST R1, P1R1
+    ST R2, P1R2
+    ST R3, P1R3
+    ST R4, P1R4
+    ST R5, P1R5
+    ST R7 P1R7
+    LDR R1, R6, #0
+    ADD R6, R6, #1
+    ST R1, P1PC
+    LDR R1, R6, #0
+    ADD R6, R6, #1
+    ST R1, P1PSR
+    LDI R1, KBIV
+    ST R1, SAVEIV
+    LEA R1, ISR180
+    STI R1, KBIV
+    LD R1, KBIMASK
+    STI R1, KBSR
+    ST R1, P2PC
+    ST R2, P2PSR
+    ADD R6, R6, #-1
+    STR R2, R6, #0
+    ADD R6, R6, #-1
+    STR R1, R6, #0
+    LD R0, P2R0
+    LD R1, P2R1
+    LD R2, P2R2
+    LD R3, P2R3
+    LD R4, P2R4
+    LD R5, P2R5
+    LD R7, P2R7
+    RTI
 
 ; Responds to a keyboard interrupt:
 ;  0. Save state of Program 2
@@ -21,8 +52,41 @@ TRAP26                  ; TODO: Implement this service routine.
 ;  3. Places the typed character in R0.
 ;  4. Returns to the caller of TRAP26.
 ;  5. Load state of Program 1
-ISR180                  ; TODO: Implement this service routine.
-
+ISR180:                  ; TODO: Implement this service routine.
+    ST R0, P2R0
+    ST R1, P2R1
+    ST R2, P2R2
+    ST R3, P2R3
+    ST R4, P2R4
+    ST R5, P2R5
+    ST R7, P2R7
+    LDR R1, R6, #0
+    ADD R6, R6, #1
+    ST R1, P2PC
+    LDR R1, R6, #0
+    ADD R6, R6, #1
+    ST R1, P2PSR
+    AND R1, R1, #0
+    STI R1, KBSR
+    LD R1, SAVEIV
+    STI R1, KBIV
+    LDI R0, KBDR
+    LD R1, P1PC
+    LD R2, P1PSR
+    ADD R6, R6, #-1
+    STR R2, R6, #0
+    ADD R6, R6, #-1
+    STR R1, R6, #0
+    LD R1, SAVEIV 
+    STI R1, KBIV
+    LD R1, P1R1
+    LD R2, P1R2
+    LD R3, P1R3
+    LD R4, P1R4
+    LD R5, P1R5
+    LD R7, P1R7
+    RTI
+    
 
 ; Program 1's data:
 P1R1    .FILL x0000     ; TODO: Use these memory locations to save and restore
